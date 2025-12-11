d133ab4877ffeb9fb6217c6f3871cd9f391d43b8
kuzbo <kuzboris2111@gmail.com>
2025-12-12 01:44:06 +0300

    изучил теорию по html продолжаю изучать

--- learning.py
+++ learning.py
@@ -1,11 +1,28 @@
-import uvicorn
-from fastapi import FastAPI
-
+import jinja2
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"id": id}
+    )
+
 @app.get("/")
 async def root():
-	return {"message": "Hello World"}
-if __name__=="__main__":
-	uvicorn.run	("main:app", host="0.0.0.0", port=800, reload=True)
+    return RedirectResponse(url="/static/mysite.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=5000)
