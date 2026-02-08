935e20e6e98070b63afa6b70a65e04d4bcfc930a
kuzbo <kuzboris2111@gmail.com>
2026-02-08 19:14:52 +0300

    узнал как работать с JS сделал минимальную версию своего приложения. Буду приступать к созданию своего проекта. и конечно еще надо подучить теорию.

--- learning.py
+++ learning.py
@@ -1,8 +1,8 @@
-import jinja2
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from starlette.responses import RedirectResponse
 
 app = FastAPI()
@@ -25,4 +25,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="127.0.0.2", port=5000)
